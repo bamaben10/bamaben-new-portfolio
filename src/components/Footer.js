4f3bf0635cc3cff397ccd04e63bebfb1406e6b44
Benjamin Salmon <bensalmon8@gmail.com>
2022-11-06 17:25:22 -0600

    project page

---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+import "./FooterStyles.css";
+
+import React from "react";
+
+import {
+  FaHome,
+  FaPhone,
+  FaMailBulk,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>Birmingham, AL</p>
+              <p>USA</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              (123) 456-7890
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              bensalmon8@gmail.com
+            </h4>
+          </div>
+        </div>
+
+        <div className="right">
+          <h4>About me</h4>
+          <p>
+            I'm new to the developer world, but am willing to learn and grow.
+          </p>
+          <div className="socail">
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "2rem" }}
+            />
+            <FaGithub
+              size={30}
+              style={{ color: "#fff", marginRight: "2rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
