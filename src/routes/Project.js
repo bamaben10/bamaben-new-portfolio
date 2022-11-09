29bcea5ae3d84121cac11f48c95fbeae5bea8274
Benjamin Salmon <bensalmon8@gmail.com>
2022-11-08 19:06:56 -0600

    pricing card

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,8 +2,9 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import HeroImg2 from "../components/HeroImg2";
 import Work from "../components/Work";
+import HeroImg2 from "../components/HeroImg2";
+import PricingCard from "../components/PricingCard";
 
 const Project = () => {
   return (
@@ -11,6 +12,7 @@ const Project = () => {
       <Navbar />
       <HeroImg2 heading="PROJECTS" text="Some of my recent work" />
       <Work />
+      <PricingCard />
       <Footer />
     </div>
   );
